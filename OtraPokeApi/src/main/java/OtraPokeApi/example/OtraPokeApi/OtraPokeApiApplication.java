4ed31f3d1d4d31ade5e7ee958e1ab20a9cd056b3
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-03 18:57:16 +0200

    Añadidas dependencias al POM

---
+++ OtraPokeApi/src/main/java/OtraPokeApi/example/OtraPokeApi/OtraPokeApiApplication.java
@@ -0,0 +1,13 @@
+package OtraPokeApi.example.OtraPokeApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtraPokeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtraPokeApiApplication.class, args);
+	}
+
+}
---
+++ OtraPokeApi/src/test/java/OtraPokeApi/example/OtraPokeApi/OtraPokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package OtraPokeApi.example.OtraPokeApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtraPokeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
