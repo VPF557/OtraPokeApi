d32dfbc57f3af74457eab24cc3f3e4f26c8553ca
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-03 19:41:34 +0200

    Test de comprobacion que un objeto se ha añadido corectamente hecho

--- OtraPokeApi/src/test/java/OtraPokeApi/example/OtraPokeApi/DataHandingTest.java
+++ OtraPokeApi/src/test/java/OtraPokeApi/example/OtraPokeApi/DataHandingTest.java
@@ -14,17 +14,18 @@ public class DataHandingTest
     @org.junit.Test
     public void buscarObjeto()
     {
-        //Comprobamos si un objeto se ha borrado correctamente
-        Param param = new Param("planets" , 5);
+        //Comprobamos si un objeto se ha añadido correctamente
+        Parametro paramN = new Parametro("type" , "5");
         LeerJson reader = new LeerJson();
-        ArrayList<Param> listaORG = new ArrayList<Param>();
-        ArrayList<Param>  listaNuev = reader.LeerFicheroPeticiones("Petitions.json") ;
+        ArrayList<Parametro> listaORG = reader.LeerFicheroPeticiones("Peticiones.json");
         DataHanding data = new DataHanding();
-        listaNuev = data.buscarObjeto(param.getParameter1(),param.getParameter2(),"Petitions.json");
+
+        ArrayList<Parametro> listaNuev = reader.LeerFicheroPeticiones("Peticiones.json");
+        listaNuev.add(paramN);
 
         if(listaNuev.size() == listaORG.size())
         {
-            fail("No lo ha borrado correctamente");
+            fail("No se ha añadido correctamente");
         }
         {
             assertTrue(true);
