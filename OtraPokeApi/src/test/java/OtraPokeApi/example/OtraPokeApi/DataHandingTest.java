e64977e3e4adccddc20b20ac2a57d39e31195f21
Pedro Paniagua <pedrojosepaniagua01@gmail.com>
2023-07-03 19:36:52 +0200

    Creada clase de test

---
+++ OtraPokeApi/src/test/java/OtraPokeApi/example/OtraPokeApi/DataHandingTest.java
@@ -0,0 +1,35 @@
+package OtraPokeApi.example.OtraPokeApi;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class DataHandingTest
+{
+    @Test
+    void contextLoads() {
+    }
+    @org.junit.Test
+    public void buscarObjeto()
+    {
+        //Comprobamos si un objeto se ha borrado correctamente
+        Param param = new Param("planets" , 5);
+        LeerJson reader = new LeerJson();
+        ArrayList<Param> listaORG = new ArrayList<Param>();
+        ArrayList<Param>  listaNuev = reader.LeerFicheroPeticiones("Petitions.json") ;
+        DataHanding data = new DataHanding();
+        listaNuev = data.buscarObjeto(param.getParameter1(),param.getParameter2(),"Petitions.json");
+
+        if(listaNuev.size() == listaORG.size())
+        {
+            fail("No lo ha borrado correctamente");
+        }
+        {
+            assertTrue(true);
+        }
+
+    }
+
+}
